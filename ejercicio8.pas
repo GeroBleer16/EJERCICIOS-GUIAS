1b4fcd3d5a0c120f12b473c389abaeb1333d2bc7
GeroBleer16 <63480253+GeroBleer16@users.noreply.github.com>
2022-08-26 21:27:37 -0300

    Add files via upload

---
+++ ejercicio8.pas
@@ -0,0 +1,10 @@
+program ejercicio8;
+   var
+     carta1,carta2,carta3,palo:string;
+begin
+  writeln('ingrese de que palo son sus cartas');
+  readln(carta1,carta2,carta3);
+  if carta1=carta2 and carta2=carta3 then
+  writeln
+end.
+
